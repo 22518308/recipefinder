324ea41b579272854d23c1a68a7085a5ecf55146
bkarp <bkarp@DESKTOP-HL8JT8O>
2025-05-02 03:13:56 +0100

    New home page text

--- obj/Release/net8.0/RecipeFinda.AssemblyInfo.cs
+++ obj/Release/net8.0/RecipeFinda.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecipeFinda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+239bc0e5d458c21cb8be0f466a1f496362f587e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e17b21af8321a866084c552db36ad455c9c61412")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecipeFinda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecipeFinda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
